1f91dbe365a7d63e2d8a77d9ce06ca589a253cb1
Akshata-M09 <89059087+Akshata-M09@users.noreply.github.com>
2025-05-08 09:40:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import HomePage from "./components/HomePage";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
